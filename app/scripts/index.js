ceccffa5d788d59dd09d4aabc6544ac11fff913b
Benjamin Phillips <benjamingt537@gmail.com>
2023-02-05 00:55:26 -0500

    definetly spagetti code but it works ill clean it up later tho

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -26,41 +26,32 @@ function openProfile() {
 generateTaskbar()
 
 dragElement(document.getElementById('page-profile'))
+dragElement(document.getElementById('page-home'))
 
 function dragElement(element) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0
-    document.getElementById('page-header').onmousedown = dragMouseDown
+    console.log(element.id);
+    document.getElementById(`${element.id}-header`).onmousedown = dragMouseDown
 
     function dragMouseDown(e) {
         console.log(e)
         e.preventDefault()
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX
+        pos3 = e.clientX // get mouse position
         pos4 = e.clientY
-        document.onmouseup = closeDragElement
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag
-    }
-
-    function elementDrag(e) {
-        console.log(e)
-        e.preventDefault()
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX
-        pos2 = pos4 - e.clientY
-        pos3 = e.clientX
-        pos4 = e.clientY
-        // set the element's new position:
-        console.log(element.offsetTop);
-        if (element.offsetTop - pos2 >= 0 && element.offsetTop - pos2 <= (document.body.clientHeight - 95) && element.offsetLeft - pos1 >= 0 && element.offsetLeft - pos1 <= (document.body.clientWidth - 100)) {
-            element.style.top = (element.offsetTop - pos2) + "px"
-            element.style.left = (element.offsetLeft - pos1) + "px"
+        document.onmouseup = () => { // stop moving if the mouse is released
+            document.onmouseup = null
+            document.onmousemove = null
         }
-    }
-
-    function closeDragElement() {
-        /* stop moving when mouse button is released:*/
-        document.onmouseup = null
-        document.onmousemove = null
+        document.onmousemove = (e) => { // function is called when mouse is moved
+            e.preventDefault()
+            pos1 = pos3 - e.clientX // caculate mouse position / offset before moving the element
+            pos2 = pos4 - e.clientY
+            pos3 = e.clientX
+            pos4 = e.clientY
+            if (element.offsetTop - pos2 >= 0 && element.offsetTop - pos2 <= (document.body.clientHeight - 95) && element.offsetLeft - pos1 >= 0 && element.offsetLeft - pos1 <= (document.body.clientWidth - element.clientWidth)) {
+                element.style.top = (element.offsetTop - pos2) + "px"
+                element.style.left = (element.offsetLeft - pos1) + "px"
+            }
+        }
     }
 }
